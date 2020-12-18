232638fb98108fd1509b944545b3e1f629d12371
Mark Powers <markppowers0@gmail.com>
2020-12-18 16:07:21 -0600

    Add music button

--- link-manager.js
+++ link-manager.js
@@ -1,11 +1,11 @@
 document.getElementById("wiki").href = `${WIKI_URL}`;
 document.getElementById("rss").href = `${RSS_URL}`;
 document.getElementById("rssvideo").href = `${RSS_VIDEO_URL}`;
-document.getElementById("gitea").href = `${GITEA_URL}`;
+document.getElementById("git").href = `${GIT_URL}`;
 document.getElementById("files").href = `${FILES_URL}`;
 document.getElementById("filefeed").href = `${FILE_FEED_URL}`;
 document.getElementById("calendar").href = `${CALENDAR_URL}`;
 document.getElementById("budget").href = `${BUDGET_URL}`;
 document.getElementById("chess").href = `${CHESS_URL}`;
 document.getElementById("wunderground").href = `https://www.wunderground.com/forecast/${WUNDERGROUND_STATION}`
-
+document.getElementById("music").href = `${MUSIC_URL}`;
